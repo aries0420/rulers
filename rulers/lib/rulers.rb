3f607fad3c87aab234d2bc105caa68f36b16ce3c
K C <aries0420@gmail.com>
2013-11-05 23:53:25 -0800

    Chapter 4 Rendering Views

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -3,6 +3,7 @@ require "rulers/routing"
 require "rulers/array"
 require "rulers/util"
 require "rulers/dependencies"
+require "rulers/controller"
 
 module Rulers
   class Application
@@ -31,16 +32,6 @@ module Rulers
         text = "</ul></body></html>"
       end
       [200, {'Content-Type' => 'text/html'}, [text]]
-    end
-  end
-
-  class Controller
-    def initialize(env)
-      @env = env
-    end
-
-    def env
-      @env
     end
   end
 end
